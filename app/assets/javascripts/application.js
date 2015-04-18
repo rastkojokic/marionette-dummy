8761ac094577159656b2e955a7788d64bcef02b6
Rastko Jokic <rastko@renderedtext.com>
2015-04-18 11:27:47 +0000

    Fix marionette bind with backbone

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require underscore
+//= require backbone
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
 //= require backbone.marionette
 
